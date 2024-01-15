f0a1d2d7430a95d99b6ed7e9868300d29e53c83b
Abu Bakr <b.xanxadjayev@gmail.com>
2024-01-15 17:34:40 +0500

    All methods are created

--- src/repository/StudentRepo.java
+++ src/repository/StudentRepo.java
@@ -1,4 +1,27 @@
 package repository;
 
-public class StudentRepo {
+import entity.Student;
+
+import java.util.List;
+
+public class StudentRepo implements Repository<Student> {
+    @Override
+    public void save(Student student) {
+
+    }
+
+    @Override
+    public void update(Student student) {
+
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(Student student) {
+
+    }
 }
